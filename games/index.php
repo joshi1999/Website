78aef92b0b1baf32d2d92a36cc99aa1940d720e2
Joshi1999 <joshi1999@yahoo.de>
2017-04-04 17:20:44 +0200

    Create index.php

---
+++ games/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>games</title>
+  </head>
+  <body>
+    <a href="Space-Shooter-1.0.1.jar">
+      Space-Shooter
+    </a><br>
+    Released: 2017/04/04<br>
+    Developer: joshi1999<br>
+    Language: Java<br>
+    Type: Game<br>
+    Genre: Sci-fi<br>
+  </body>
+</html>
